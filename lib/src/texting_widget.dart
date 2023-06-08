b419f8e9415344b7598073d931208f9875d9d593
Aladdin16659 <aladdin16659@gmail.com>
2023-06-08 12:16:36 +0300

    push
    Signed-off-by: Aladdin16659 <aladdin16659@gmail.com>

--- lib/src/texting_widget.dart
+++ lib/src/texting_widget.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Texting extends StatefulWidget {
-  const Texting({super.key});
+  const Texting({
+    super.key,
+    required this.controller,
+    this.maxHeight,
+    this.minHeight,
+  });
+  final TextEditingController controller;
+  final double? maxHeight;
+  final double? minHeight;
 
   @override
   State<Texting> createState() => _TextingState();
@@ -11,9 +19,13 @@ class _TextingState extends State<Texting> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 400,
-      color: Colors.red,
+      constraints: BoxConstraints(
+        maxHeight: widget.maxHeight ?? 48,
+        minHeight: widget.minHeight ?? 48,
+      ),
+      child: TextFormField(
+        controller: widget.controller,
+      ),
     );
   }
 }
